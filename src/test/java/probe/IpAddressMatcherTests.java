b6c482f6c8acf629c90b204cabcaed8146393b2b
levry <lriskov@gmail.com>
2025-07-09 10:49:17 +0500

    More examples

--- src/test/java/probe/IpAddressMatcherTests.java
+++ src/test/java/probe/IpAddressMatcherTests.java
@@ -3,6 +3,7 @@ package probe;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.security.web.util.matcher.IpAddressMatcher;
 import probe.net.DetectResolvingHostException;
@@ -46,7 +47,24 @@ class IpAddressMatcherTests {
         var matcher = new IpAddressMatcher("10.1.1.0/24");
 
         assertThatException().isThrownBy(() -> matcher.matches("www"))
-                .withMessage("ipAddress 10x1x1x1 doesn't look like an IP Address. Is it a host name?");
+                .withMessage("ipAddress www doesn't look like an IP Address. Is it a host name?");
+    }
+
+    @CsvSource({
+            "10.1.2, 10.1.0.2, true",
+            "10.1.0.2, 10.1.2, true",
+            "10.1, 10.0.0.1, true",
+            "10.1/16, 10.0.123.1, true",
+            "10.1/16, 10.0.123.23, true",
+            "10.1/16, 10.3.123.23, false",
+            "10.0/24, 10.0.0.12, true",
+            "10.0/24, 10.0.1.12, false",
+    })
+    @ParameterizedTest
+    void matchesWhenIpV4(String matcherAddress, String ipAddress, boolean valid) {
+        var matcher = new IpAddressMatcher(matcherAddress);
+
+        assertThat(matcher.matches(ipAddress)).isEqualTo(valid);
     }
 
     @Test
